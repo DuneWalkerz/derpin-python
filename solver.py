d1cb50113420e6bc9b7367f1c85649ab2f983e80
Dune@work <dunewalkerz@gmail.com>
2022-05-25 16:59:10 +0300

    valid fixed had bugs not to sure what was wronge tho will need to cheak latter

--- solver.py
+++ solver.py
@@ -13,28 +13,26 @@ board = [
 [0,8,0,0,3,7,0,0,0]
 ]
 
+
 def solve(bo):
-    
-    find = find_empty_square(bo)
+    find = find_empty(bo)
     if not find:
         return True
     else:
-        row, col = find # sets row,col to the location of an empty square
+        row, col = find
 
-    for i in range (1,10): 
-        if valid(bo, i, (row,col)):
+    for i in range(1,10):
+        if valid(bo, i, (row, col)):
             bo[row][col] = i
 
             if solve(bo):
                 return True
 
             bo[row][col] = 0
-    
+
     return False
 
 
-# co_ordinate in this case is an (x,y) cordinate value for defineing look location. this will validate if the num at that square is valid by cheaking row, col and 3 x 3 square
-#numeral is value being tested
 def valid(bo, num, pos):
     # Check row
     for i in range(len(bo[0])):
@@ -47,7 +45,6 @@ def valid(bo, num, pos):
             return False
 
     # Check box
-    #when using // it tells you the remaider integer eg 6//3 = 2 0-2 = 0 3-5 = 1 6-8 = 2 this lest you find the quadrent
     box_x = pos[1] // 3
     box_y = pos[0] // 3
 
@@ -59,25 +56,22 @@ def valid(bo, num, pos):
     return True
 
 
-
-def board_print(bo):
-
-    for i in range(len(bo)): # defines steps of function from rows (0,9)
+def print_board(bo):
+    for i in range(len(bo)):
         if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - - ")
-        
-        for j in range(len(bo[0])):
-            if j % 3 == 0 and j != 0 :
-                print(" | ", end="") 
+            print("- - - - - - - - - - - - - ")
 
-            if j == 8:              # this is the final line as 0 - 8 is 1-9 0=1 in this case
-                print(bo[i][j])  # no need for an added space " " 
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
             else:
                 print(str(bo[i][j]) + " ", end="")
 
-#board_print(board)
 
-def find_empty_square(bo):
+def find_empty(bo):
     for i in range(len(bo)):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
@@ -85,7 +79,7 @@ def find_empty_square(bo):
 
     return None
 
-board_print(board)
+print_board(board)
 solve(board)
 print("___________________")
-board_print(board)
+print_board(board)
